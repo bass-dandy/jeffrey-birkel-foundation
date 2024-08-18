468669f92f0820c4e99d98e34a670471da253dd3
bass-dandy <ctdinh93@gmail.com>
2024-08-17 20:00:11 -0400

    set up sveltekit and gh actions

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<main>
+	<div class="banner">
+		<img src="construction.gif" alt="" />
+		<h1>Jeffrey Birkel Foundation</h1>
+		<img src="construction.gif" alt="" />
+	</div>
+</main>
+
+<style>
+	.banner {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 24px;
+	}
+
+	.banner img {
+		height: 64px;
+		width: auto;
+	}
+</style>
