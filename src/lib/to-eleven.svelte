3cbf071ea613f6633e2fc5a73dbd7a9893e7346b
bass-dandy <ctdinh93@gmail.com>
2024-08-23 13:17:49 -0400

    optimize images a bit

--- src/lib/to-eleven.svelte
+++ src/lib/to-eleven.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
 	import ContentContainer from './content-container.svelte';
+
+	const images = [{
+		src: 'images/chicago-2004.jpg',
+		alt: 'Jeff smiling while crossing the finish line at the 2004 Chicago Marathon',
+	}, {
+		src: 'images/cat.jpg',
+		alt: 'Jeff smiling with his arms behind his back in a bright yellow running tank with the Cat logo on the front.',
+	}, {
+		src: 'images/boston-2005.jpg',
+		alt: 'Jeff crossing the finish line at the 2005 Boston Marathon. He is smiling but he was exhausted!',
+	}];
 </script>
 
 <div class="to-eleven">
 	<div class="top">
 		<ContentContainer>
 			<div class="images">
-				<img src="images/chicago-2004.jpeg" alt="" />
-				<img src="images/cat.jpeg" alt="" />
-				<img src="images/boston-2005.jpeg" alt="" />
+				{#each images as { src, alt }}
+					<img
+						width="752"
+						height="1056"
+						loading="lazy"
+						{src}
+						{alt}
+					/>
+				{/each}
 			</div>
 		</ContentContainer>
 	</div>
@@ -28,24 +45,20 @@
 <style lang="scss">
 	@import '../mixins.scss';
 
-	.to-eleven {
-		position: relative;
-	}
-
 	.top {
 		position: relative;
-		margin-bottom: -1px; /* to hide the subpixel seam between the two sections */
-	}
+		margin-bottom: -1px; // to hide the subpixel seam between the two sections
 
-	.top::after {
-		content: '';
-		position: absolute;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		height: 50%;
-		background: var(--color-secondary);
-		z-index: 0;
+		&::after {
+			content: '';
+			position: absolute;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			height: 50%;
+			background: var(--color-secondary);
+			z-index: 0;
+		}
 	}
 
 	.images {
@@ -55,15 +68,26 @@
 		gap: var(--spacing-sm);
 		max-width: 800px;
 		margin: auto;
+
+		 img {
+			display: block;
+			flex: 1;
+			min-width: 0px;
+			height: auto;
+			border: 5px solid white;
+			box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+			z-index: 1;
+		}
 	}
 
-	.images img {
-		display: block;
-		min-width: 0px;
-		flex: 1;
-		border: 5px solid white;
-		box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
-		z-index: 1;
+	@include breakpoint-xs {
+		.images {
+			gap: var(--spacing-md);
+
+			img:nth-child(2) {
+				flex: 1.2;
+			}
+		}
 	}
 
 	.bottom {
@@ -71,25 +95,15 @@
 		color: var(--color-white);
 		padding: var(--spacing-lg) 0 var(--spacing-xl);
 		text-align: center;
-	}
 
-	.bottom h2 {
-		margin: 0;
+		h2 {
+			margin: 0;
+		}
 	}
 
 	.body {
 		margin-top: var(--spacing-lg);
 		font-size: 3rem;
 		font-family: economica;
-	}
-
-	@include breakpoint-xs {
-		.images {
-			gap: var(--spacing-md);
-		}
-
-		.images img:nth-child(2) {
-			flex: 1.2;
-		}
 	}
 </style>
