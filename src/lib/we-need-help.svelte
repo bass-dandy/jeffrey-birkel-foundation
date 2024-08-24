d9a3cba4128acfe32a49a0939e3787f2c203538a
bass-dandy <ctdinh93@gmail.com>
2024-08-24 14:06:10 -0400

    lfg boys

--- src/lib/we-need-help.svelte
+++ src/lib/we-need-help.svelte
@@ -10,7 +10,7 @@
 			We started the Jeffrey F. Birkel Foundation to fundraise for the ALS Association, ALS United, and ALS Research. Before his diagnosis, Jeff was an avid distance runner. He's run 10 marathons and one Grand Canyon Rim-to-Rim! In 2005, Jeff ran the Boston Marathon and helped raise funds for MPD research, a rare disease with which a dear friend and co-worker had been diagnosed. Now Jeff wants to turn his diagnosis into an opportunity to raise funds for ALS, which, like MPD, is rare and under-funded.
 		</p>
 		<p>
-			Jeff encouraged his two boys, Matt and Mike, to take up running at a young age - and they did! They both ran Boston in 2023, and on Father’s Day of 2024 they decided they'd like to try to run it again with Jeff - one of them pushing him in a racing pushchair and the other running alongside. The Boston Marathon requires setting an acceptable qualifying time at another marathon to get an entry, so they've decided their qualifying race will be the Nebraska State Fair Marathon on August 24th.
+			Jeff encouraged his two boys, Matt and Mike, to take up running at a young age - and they did! They both ran Boston in 2023, and on Father’s Day of 2024 they decided they'd like to try to run it again with Jeff - one of them pushing him in a racing pushchair and the other running alongside. The Boston Marathon requires setting an acceptable qualifying time at another marathon to get an entry, and they did just that at the Nebraska State Fair Marathon on August 24th!
 		</p>
 	</ContentContainer>
 </section>
