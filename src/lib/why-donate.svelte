0d42593534955f4aba954ae1c14a04e4d67edd29
bass-dandy <ctdinh93@gmail.com>
2024-08-26 16:42:46 -0400

    tweak fonts and copy

--- src/lib/why-donate.svelte
+++ src/lib/why-donate.svelte
@@ -6,7 +6,7 @@
 <section>
 	<ContentContainer>
 		<SectionTitle>Why donate?</SectionTitle>
-		Since all funds raised by the Jeffrey F. Birkel Foundation will eventually be donated to either ALS United or the ALS Association, you might be wondering: why not donate to those charities directly? Well, if Matt and Mike qualify for the Boston Marathon, we'll still need to apply for a separate pushchair entry. To get a charitable bib entry, we'll likely need to raise over $20,000 by November. Time is precious; though we've set our sights on Boston 2024, donating to the Jeffrey F. Birkel Foundation allows us to save up and try again in 2025 if we fail to get a bib this year. In addition, incorporating as a 501(c)(3) non-profit corporation ensures all your donations will be tax-exempt and meet the requirements of companies with matching gift programs.
+		Since all funds raised by the Jeffrey F. Birkel Foundation will eventually be donated to either ALS United or the ALS Association, you might be wondering: why not donate to those charities directly? Well, even though Matt and Mike have qualified for the Boston Marathon, we still need to apply for a separate pushchair entry. To get a charitable bib entry, we'll likely need to raise over $20,000 by November. Time is precious; though we've set our sights on Boston 2024, donating to the Jeffrey F. Birkel Foundation allows us to save up and try again in 2025 if we fail to get a bib this year. In addition, incorporating as a 501(c)(3) non-profit corporation ensures all your donations will be tax-exempt and meet the requirements of companies with matching gift programs.
 	</ContentContainer>
 </section>
 
