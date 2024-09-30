46d7c14bd7db5e2f1dde909bf87ec724355a6613
bass-dandy <ctdinh93@gmail.com>
2024-09-30 17:25:09 -0400

    add sunflower hour video and facades

--- src/lib/media.svelte
+++ src/lib/media.svelte
@@ -1,38 +1,29 @@
 <script lang="ts">
-	import ArrowUpRightFromSquare from '$svg/arrow-up-right-from-square-solid.svg?component';
 	import ContentContainer from './content-container.svelte';
 	import SectionTitle from './section-title.svelte';
+	import Video from './video.svelte';
 </script>
 
 <section>
 	<ContentContainer>
 		<SectionTitle>Media</SectionTitle>
-		<h3>
-			<a
-				href="https://www.knopnews2.com/2024/08/23/man-with-als-looks-qualify-boston-marathon-with-two-sons/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				KNOP News: Man with ALS looks to qualify for Boston Marathon with two sons
-				<ArrowUpRightFromSquare
-					fill="currentColor"
-					height="1rem"
-					style="margin-left: var(--spacing-xs); position: relative; top: 2px;"
+		<ul>
+			<li>
+				<Video
+					title="KNOP News: Man with ALS looks to qualify for Boston Marathon with two sons"
+					src="https://www.youtube.com/embed/oWJOKq-2vmQ?si=C6WW1rcrNUq80v-I"
+					href="https://www.knopnews2.com/2024/08/23/man-with-als-looks-qualify-boston-marathon-with-two-sons/"
+					facade="images/knop-facade.jpg"
 				/>
-			</a>
-		</h3>
-		<div class="video">
-			<iframe
-				width="560"
-				height="315"
-				src="https://www.youtube.com/embed/oWJOKq-2vmQ?si=C6WW1rcrNUq80v-I"
-				title="YouTube video player"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				referrerpolicy="strict-origin-when-cross-origin"
-				allowfullscreen
-			/>
-		</div>
+			</li>
+			<li>
+				<Video
+					title="Pushing Forward: Jeff Birkel’s Fight and Marathon Mission"
+					src="https://www.youtube.com/embed/Yxu-3mQ_XK8?si=qFnw7hY5dV9Y1f_q"
+					facade="images/sunflower-facade.jpg"
+				/>
+			</li>
+		</ul>
 	</ContentContainer>
 </section>
 
@@ -41,22 +32,17 @@
 		padding: var(--spacing-xl) 0 0;
 	}
 
-	h3 {
-		margin: 0 0 var(--spacing-xs) 0;
-		font-size: 1.125rem;
-		font-weight: normal;
+	ul {
+		display: flex;
+		flex-direction: column;
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		gap: var(--spacing-lg);
 	}
 
-	a:hover {
-		text-decoration: none;
-	}
-
-	.video {
-		aspect-ratio: 16 / 9;
-	}
-
-	iframe {
-		width: 100%;
-		height: 100%;
+	li {
+		margin: 0;
+		padding: 0;
 	}
 </style>
---
+++ src/lib/video.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import ArrowUpRightFromSquare from '$svg/arrow-up-right-from-square-solid.svg?component';
+	import Play from '$svg/play-solid.svg?component';
+
+	export let title: string;
+	export let src: string;
+	export let href: string;
+	export let facade: string;
+
+	let isMounted = false;
+</script>
+
+<h3>
+	{#if href}
+	<a
+		href={href}
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		{title}
+		<ArrowUpRightFromSquare
+			fill="currentColor"
+			height="1rem"
+			style="margin-left: var(--spacing-xs); position: relative; top: 2px;"
+		/>
+	</a>
+	{:else}
+		{title}
+	{/if}
+</h3>
+<div class="video">
+	{#if !facade || isMounted}
+		<iframe
+			width="560"
+			height="315"
+			src={`${src}${facade ? '&autoplay=1' : ''}`}
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+		/>
+	{:else}
+		<img src={facade} alt="" loading="lazy" />
+		<button on:click={() => isMounted = true}>
+			<Play
+				fill="currentColor"
+				height="4rem"
+				aria-label="play video"
+			/>
+		</button>
+	{/if}
+</div>
+
+<style>
+	h3 {
+		margin: 0 0 var(--spacing-xs) 0;
+		font-size: 1.125rem;
+		font-weight: normal;
+	}
+
+	a:hover {
+		text-decoration: none;
+	}
+
+	.video {
+		aspect-ratio: 16 / 9;
+		position: relative;
+	}
+
+	iframe {
+		width: 100%;
+		height: 100%;
+	}
+
+	img {
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
+
+	button {
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background: rgba(0, 0, 0, 0.5);
+		color: var(--color-white);
+		border: 0;
+		cursor: pointer;
+	}
+</style>
