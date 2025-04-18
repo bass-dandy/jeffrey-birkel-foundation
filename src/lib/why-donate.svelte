6902830dc7d5bbbcf7d03e2f7b73c671a59ca7d1
bass-dandy <ctdinh93@gmail.com>
2025-04-17 23:46:05 -0400

    update copy

--- src/lib/why-donate.svelte
+++ src/lib/why-donate.svelte
@@ -6,7 +6,7 @@
 <section>
 	<ContentContainer>
 		<SectionTitle>Why donate?</SectionTitle>
-		In order to get a pushchair (“Duo Team”) entry, the Birkel family needed to go through the Boston Marathon’s charity program and fundraise a certain amount. We’re proud to announce that in December 2024, The ALS Association and the Boston Athletic Association granted entry bibs to all three for the 129th running of the Boston Marathon on April 21st, 2025! We’re also happy to report that in November, the Foundation achieved its original donation goal of $20,000 for the ALS Association, thus meeting their fundraising commitment! Many thanks to our generous donors! There’s no reason to stop there, though. We want to continue fundraising as much as we can, all the way up until the marathon finish line!
+		In order to get a pushchair (“Duo Team”) entry, the Birkel family needed to go through the Boston Marathon’s charity program and fundraise a certain amount. We’re proud to announce that in December 2024, The ALS Association and the Boston Athletic Association granted entry bibs to all three for the 129th running of the Boston Marathon on April 21st, 2025! We’re also happy to report that in November, the Foundation had achieved its original goal of $20,000 to the ALS Association meeting their fundraising commitment! Many thanks to our generous donors! There’s no reason to stop there, though. We want to continue fundraising as much as we can, all the way up until the marathon finish line!
 	</ContentContainer>
 </section>
 
