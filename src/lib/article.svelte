8c04c33d25cdc46d4ab26fd7eaaaefd02c35b3f6
bass-dandy <ctdinh93@gmail.com>
2025-08-07 02:39:18 -0400

    add Article component, update media

---
+++ src/lib/article.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import ArrowUpRightFromSquare from '$svg/arrow-up-right-from-square-solid.svg?component';
+
+	export let title: string;
+	export let date: string;
+	export let href: string;
+	export let thumbnail: string;
+</script>
+
+<div>
+	{#if date}
+		<div class="headline">{date}</div>
+	{/if}
+	<a href={href} target="_blank" rel="noopener noreferrer">
+		<img src={thumbnail} alt="" loading="lazy" class="thumbnail" />
+		<h3>{title}</h3>
+	</a>
+</div>
+
+<style>
+	.headline {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	h3 {
+		margin: 0 0 var(--spacing-xs) 0;
+		font-size: 1.125rem;
+		font-weight: bold;
+	}
+
+	a:hover {
+		text-decoration: none;
+	}
+
+	.thumbnail {
+		display: block;
+		aspect-ratio: 16 / 9;
+		border: 1px solid var(--color-primary);
+		width: 100%;
+		height: 100%;
+		box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0);
+		transition: box-shadow 0.15s, transform 0.15s, border-color 0.15s;
+	}
+
+	a:hover .thumbnail {
+		box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.15);
+		transform: scale(1.01);
+		border-color: blue;
+	}
+</style>
--- src/lib/media.svelte
+++ src/lib/media.svelte
@@ -1,66 +1,114 @@
 <script lang="ts">
+	import Collapse from './collapse.svelte';
 	import ContentContainer from './content-container.svelte';
 	import SectionTitle from './section-title.svelte';
+	import Article from './article.svelte';
 	import Video from './video.svelte';
+
+	let isExpanded = false;
 </script>
 
 <section>
 	<ContentContainer>
 		<SectionTitle>Media</SectionTitle>
-		<ul>
-			<li>
-				<Video
-					title="How a Minnesota son is helping his father run the Boston Marathon"
-					date="3/16/25"
-					src="https://www.youtube.com/embed/HHkp_w9TJ-o?si=fWlkyHywHJMDzoAo"
-					facade="images/cbs-facade.png"
-					link={{
-						href: 'https://www.cbsnews.com/minnesota/video/how-a-minnesota-son-is-helping-his-father-run-the-boston-marathon/',
-						text: 'full story on CBS',
-					}}
-				/>
-			</li>
-			<li>
-				<Video
-					title="ALS United: Team ALS 2025 Kickoff - Birkel Family Story"
-					date="3/11/25"
-					src="https://www.youtube.com/embed/pavYGm6aut0?si=-l23zj_yEkxjmpob"
-					facade="images/als-united-kickoff-facade.png"
-				/>
-			</li>
-			<li>
-				<Video
-					title="Father diagnosed with ALS and son prepare for Boston Marathon"
-					date="1/31/25"
-					src="https://www.youtube.com/embed/cpp3hzKAzXM?si=-HZ5aptRAk-gK8lb"
-					facade="images/kttc-facade.png"
-					link={{
-						href: 'https://www.kttc.com/2025/02/01/father-diagnosed-with-als-son-prepare-boston-marathon/',
-						text: 'full story on KTTC',
-					}}
-				/>
-			</li>
-			<li>
-				<Video
-					title="Pushing Forward: Jeff Birkel’s Fight and Marathon Mission"
-					date="9/17/24"
-					src="https://www.youtube.com/embed/LjZsrBlYs_4?si=_dF383VqecjNJ230"
-					facade="images/sunflower-facade.jpg"
-				/>
-			</li>
-			<li>
-				<Video
-					title="Man with ALS looks to qualify for Boston Marathon with two sons"
-					date="8/23/24"
-					src="https://www.youtube.com/embed/KlAgsteqJ18?si=QG0VEP451Q_bblJb"
-					facade="images/knop-facade.jpg"
-					link={{
-						href: 'https://www.knopnews2.com/2024/08/23/man-with-als-looks-qualify-boston-marathon-with-two-sons/',
-						text: 'full story on KNOP',
-					}}
-				/>
-			</li>
-		</ul>
+		<Collapse bind:isExpanded={isExpanded}>
+			<ul>
+				<li>
+					<Article
+						title="The Phil Luciano Show: Peorian Jeffrey Birkel, an avid runner, shares incredible story of perseverance after being diagnosed with ALS in 2023"
+						date="6/10/25"
+						href="https://omny.fm/shows/the-phil-luciano-show/peorian-jeffrey-birkle-an-avid-runner-shares-incredible-story-of-perseverance-after-being-diagnosed-with-als-in-2023"
+						thumbnail="images/media/phil-luciano.webp"
+					/>
+				</li>
+				<li>
+					<Article
+						title=" Peoria Journal Star: ‘Heaven on earth’: How a Peoria native with ALS finished 2025 Boston Marathon with his sons"
+						date="5/25/25"
+						href="https://www.pjstar.com/story/sports/college/cross-country/2025/05/20/2025-boston-marathon-peoria-native-with-als-teams-with-sons-for-race/83688330007/"
+						thumbnail="images/media/journal-star.webp"
+					/>
+				</li>
+				<li>
+					<Video
+						title="ALS Matters: Episode 6 - Running the Boston Marathon as a Family"
+						date="5/21/25"
+						src="https://www.youtube.com/embed/e3AsKrFutWg?si=Vzk6jniIbOs9VFx_"
+						facade="images/media/05-21-25-facade.jpg"
+					/>
+				</li>
+				<li>
+					<Video
+						title="WCCO News: These Minnesota brothers helped their dad with ALS run the Boston Marathon, here's how"
+						date="5/18/25"
+						src="https://www.youtube.com/embed/WGAx0t3JrMk?si=WIFzdoTRpCwjObE3"
+						facade="images/media/05-18-25-facade.jpg"
+					/>
+				</li>
+				<li>
+					<Article
+						title="Boston Globe: ‘Heaven on Earth’: How one ALS patient made the Boston Marathon a family affair — and the best day of his life"
+						date="4/21/25"
+						href="https://www.bostonglobe.com/2025/04/21/sports/boston-marathon-als-runner-jeffrey-birkel/"
+						thumbnail="images/media/boston-globe.webp"
+					/>
+				</li>
+				<li>
+					<Video
+						title="How a Minnesota son is helping his father run the Boston Marathon"
+						date="3/16/25"
+						src="https://www.youtube.com/embed/HHkp_w9TJ-o?si=fWlkyHywHJMDzoAo"
+						facade="images/cbs-facade.png"
+						link={{
+							href: 'https://www.cbsnews.com/minnesota/video/how-a-minnesota-son-is-helping-his-father-run-the-boston-marathon/',
+							text: 'full story on CBS',
+						}}
+					/>
+				</li>
+				{#if isExpanded}
+					<li>
+						<Video
+							title="ALS United: Team ALS 2025 Kickoff - Birkel Family Story"
+							date="3/11/25"
+							src="https://www.youtube.com/embed/pavYGm6aut0?si=-l23zj_yEkxjmpob"
+							facade="images/als-united-kickoff-facade.png"
+						/>
+					</li>
+					<li>
+						<Video
+							title="Father diagnosed with ALS and son prepare for Boston Marathon"
+							date="1/31/25"
+							src="https://www.youtube.com/embed/cpp3hzKAzXM?si=-HZ5aptRAk-gK8lb"
+							facade="images/kttc-facade.png"
+							link={{
+								href: 'https://www.kttc.com/2025/02/01/father-diagnosed-with-als-son-prepare-boston-marathon/',
+								text: 'full story on KTTC',
+							}}
+						/>
+					</li>
+					<li>
+						<Video
+							title="Pushing Forward: Jeff Birkel’s Fight and Marathon Mission"
+							date="9/17/24"
+							src="https://www.youtube.com/embed/LjZsrBlYs_4?si=_dF383VqecjNJ230"
+							facade="images/sunflower-facade.jpg"
+						/>
+					</li>
+					<li>
+						<Video
+							title="Man with ALS looks to qualify for Boston Marathon with two sons"
+							date="8/23/24"
+							src="https://www.youtube.com/embed/KlAgsteqJ18?si=QG0VEP451Q_bblJb"
+							facade="images/knop-facade.jpg"
+							link={{
+								href: 'https://www.knopnews2.com/2024/08/23/man-with-als-looks-qualify-boston-marathon-with-two-sons/',
+								text: 'full story on KNOP',
+							}}
+						/>
+					</li>
+				{/if}
+			</ul>
+		</Collapse>
 	</ContentContainer>
 </section>
 
