82f9064b39dcff8681a5db5f5c45625f7d3b2c45
bass-dandy <ctdinh93@gmail.com>
2026-01-26 19:37:55 -0500

    use access token properly

--- script/fetch-insta.js
+++ script/fetch-insta.js
@@ -5,10 +5,6 @@ const ACCESS_TOKEN = process.env.INSTAGRAM_ACCESS_TOKEN;
 
 (async () => {
 	try {
-		// refresh access token before fetching posts
-		await fetch(
-			`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${ACCESS_TOKEN}`
-		);
 		const res = await fetch(
 			`https://graph.facebook.com/v22.0/17841472361745279/media?fields=caption%2Cmedia_url%2Cmedia_type%2Cthumbnail_url%2Ctimestamp%2Cpermalink%2Cchildren%7Bmedia_url%7D&access_token=${ACCESS_TOKEN}`
 		);
